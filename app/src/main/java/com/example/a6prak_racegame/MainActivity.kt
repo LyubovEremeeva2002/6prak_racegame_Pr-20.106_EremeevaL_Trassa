2c6eaf68183fc2ee4ed09d9620c597e651b2d9fc
Danila <terrazavr2002@gmail.com>
2021-05-26 17:20:57 +0700

    Initial commit

---
+++ app/src/main/java/com/example/a6prak_racegame/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.a6prak_racegame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
